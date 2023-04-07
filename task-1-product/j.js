dcbbc9160a51c16c107bbf65cae8668292855af3
lenovo <lenovo@LAPTOP-BLNTEBH7>
2023-04-07 10:11:22 +0530

    this the data for the api

---
+++ task-1-product/j.js
@@ -0,0 +1,213 @@
+let d = {
+  batchcomplete: "",
+  continue: { sroffset: 20, continue: "-||" },
+  query: {
+    searchinfo: {
+      totalhits: 40031,
+      suggestion: "rest",
+      suggestionsnippet: "rest",
+    },
+    search: [
+      {
+        ns: 0,
+        title: "React",
+        pageid: 862660,
+        size: 2365,
+        wordcount: 295,
+        snippet:
+          '<span class="searchmatch">REACT</span> or <span class="searchmatch">React</span> may refer to: <span class="searchmatch">REACT</span> (telescope), a telescope at Fenton Hill Observatory, New Mexico, US <span class="searchmatch">React</span> (software) , a JavaScript library for building',
+        timestamp: "2023-04-07T02:52:09Z",
+      },
+      {
+        ns: 0,
+        title: "Reactance",
+        pageid: 18751574,
+        size: 579,
+        wordcount: 95,
+        snippet:
+          '<span class="searchmatch">Reactance</span> may refer to: Electrical <span class="searchmatch">reactance</span>, the opposition to a change in voltage due to capacitance (capacitive <span class="searchmatch">reactance</span>) or in current due to inductance',
+        timestamp: "2020-05-21T11:57:31Z",
+      },
+      {
+        ns: 0,
+        title: "React (software)",
+        pageid: 44926137,
+        size: 42016,
+        wordcount: 3412,
+        snippet:
+          '<span class="searchmatch">React</span> (also known as <span class="searchmatch">React</span>.js or <span class="searchmatch">React</span>JS) is a free and open-source front-end JavaScript library for building user interfaces based on components. It is',
+        timestamp: "2023-04-03T15:14:47Z",
+      },
+      {
+        ns: 0,
+        title: "Reactable",
+        pageid: 8588746,
+        size: 9196,
+        wordcount: 1041,
+        snippet:
+          'The <span class="searchmatch">Reactable</span> is an electronic musical instrument with a tabletop tangible user interface that was developed within the Music Technology Group at the',
+        timestamp: "2022-10-09T23:56:49Z",
+      },
+      {
+        ns: 0,
+        title: "React Native",
+        pageid: 48720974,
+        size: 9294,
+        wordcount: 722,
+        snippet:
+          '<span class="searchmatch">React</span> Native is an open-source UI software framework created by Meta Platforms, Inc. It is used to develop applications for Android, Android TV, iOS,',
+        timestamp: "2023-04-04T20:06:41Z",
+      },
+      {
+        ns: 0,
+        title: "ReactOS",
+        pageid: 360889,
+        size: 78735,
+        wordcount: 6498,
+        snippet:
+          '<span class="searchmatch">React</span>OS is a free and open-source operating system for amd64/i686 personal computers intended to be binary-compatible with computer programs and device',
+        timestamp: "2023-03-25T19:56:59Z",
+      },
+      {
+        ns: 0,
+        title: "React (media franchise)",
+        pageid: 49275846,
+        size: 37851,
+        wordcount: 3726,
+        snippet:
+          '<span class="searchmatch">React</span> is a media franchise used by the Fine Brothers consisting of several online series centering on a group of individuals <span class="searchmatch">reacting</span> to viral videos',
+        timestamp: "2023-04-07T02:52:09Z",
+      },
+      {
+        ns: 0,
+        title: "Rapid React",
+        pageid: 69675446,
+        size: 24354,
+        wordcount: 1092,
+        snippet:
+          'Rapid <span class="searchmatch">React</span>, stylized as RAPID <span class="searchmatch">REACT</span> and officially known as Rapid <span class="searchmatch">React</span> presented by The Boeing Company for sponsorship reasons, is the FIRST Robotics',
+        timestamp: "2023-01-07T20:02:02Z",
+      },
+      {
+        ns: 0,
+        title: "React Media, LLC",
+        pageid: 36336637,
+        size: 57084,
+        wordcount: 4772,
+        snippet:
+          '<span class="searchmatch">React</span> Media, LLC (also known as <span class="searchmatch">React</span>; formerly as Fine Brothers Entertainment) is an American media company and group of copyright trolls, founded by',
+        timestamp: "2023-04-07T02:52:09Z",
+      },
+      {
+        ns: 0,
+        title: "Electrical reactance",
+        pageid: 140710,
+        size: 14673,
+        wordcount: 2200,
+        snippet:
+          'electrical circuits, <span class="searchmatch">reactance</span> is the opposition presented to alternating current by inductance and,or capacitance. Greater <span class="searchmatch">reactance</span> gives smaller current',
+        timestamp: "2023-03-30T22:10:21Z",
+      },
+      {
+        ns: 0,
+        title: "React Quotes",
+        pageid: 14985089,
+        size: 8876,
+        wordcount: 862,
+        snippet:
+          '&quot;<span class="searchmatch">React</span> Quotes&quot; is the fifth episode of the fifth season of the HBO original series The Wire. The episode was written by David Mills from a story by David',
+        timestamp: "2023-03-14T18:54:16Z",
+      },
+      {
+        ns: 0,
+        title: "Reactance (psychology)",
+        pageid: 3042574,
+        size: 20415,
+        wordcount: 2535,
+        snippet:
+          'In psychology, <span class="searchmatch">reactance</span> is an unpleasant motivational reaction to offers, persons, rules, or regulations that threaten or eliminate specific behavioral',
+        timestamp: "2023-04-06T20:49:22Z",
+      },
+      {
+        ns: 0,
+        title: "Deadpool and Korg React",
+        pageid: 68266160,
+        size: 15591,
+        wordcount: 1144,
+        snippet:
+          'Deadpool and Korg <span class="searchmatch">React</span> is a 2021 American superhero promotional short film featuring the Marvel Comics characters Deadpool and Korg. The film was written',
+        timestamp: "2023-01-25T06:08:30Z",
+      },
+      {
+        ns: 0,
+        title: "Radio Emergency Associated Communication Teams",
+        pageid: 862657,
+        size: 7017,
+        wordcount: 973,
+        snippet:
+          '<span class="searchmatch">REACT</span> (Radio Emergency Associated Communication Teams) began as a CB radio Emergency Channel 9 monitoring organization across the United States and Canada',
+        timestamp: "2023-01-19T11:54:48Z",
+      },
+      {
+        ns: 0,
+        title: "Stun belt",
+        pageid: 307350,
+        size: 3578,
+        wordcount: 480,
+        snippet:
+          'prisoners in the United States and elsewhere in the world. One type is the <span class="searchmatch">REACT</span> belt (see below). Some stun belts can restrain the subject\'s hands and have',
+        timestamp: "2023-04-02T23:31:10Z",
+      },
+      {
+        ns: 0,
+        title: "React (The Pussycat Dolls song)",
+        pageid: 62877005,
+        size: 99655,
+        wordcount: 8304,
+        snippet:
+          '&quot;<span class="searchmatch">React</span>&quot; is a song recorded by American girl group The Pussycat Dolls and was released by Access Records on February 7, 2020. It is the group\'s first song',
+        timestamp: "2022-09-14T09:15:15Z",
+      },
+      {
+        ns: 0,
+        title: "Chemicals React",
+        pageid: 5536129,
+        size: 11647,
+        wordcount: 809,
+        snippet:
+          '&quot;Chemicals <span class="searchmatch">React</span>&quot; is a song written and recorded by American pop rock duo Aly &amp; AJ, for the deluxe edition reissue of their debut album Into the Rush',
+        timestamp: "2022-09-22T01:50:06Z",
+      },
+      {
+        ns: 0,
+        title: "React (book)",
+        pageid: 31950338,
+        size: 4629,
+        wordcount: 480,
+        snippet:
+          '<span class="searchmatch">React</span> (from Spanish: Reacciona) is a book by Rosa Mar\u00eda Artal published in Spain in 2011 by Aguilar, which compiles articles by Jos\u00e9 Luis Sampedro, Baltasar',
+        timestamp: "2022-07-05T19:14:54Z",
+      },
+      {
+        ns: 0,
+        title: "React (band)",
+        pageid: 25548786,
+        size: 936,
+        wordcount: 109,
+        snippet:
+          '<span class="searchmatch">REACT</span> was an American boy band made up of Tim Cruz and Daniel Meteryeon. <span class="searchmatch">REACT</span> had a Top 10 hit on the dance charts with &quot;Let\'s Go All The Way&quot;. After',
+        timestamp: "2022-01-16T05:02:15Z",
+      },
+      {
+        ns: 0,
+        title: "Next.js",
+        pageid: 60172264,
+        size: 17057,
+        wordcount: 1709,
+        snippet:
+          'private company Vercel providing <span class="searchmatch">React</span>-based web applications with server-side rendering and static website generation. <span class="searchmatch">React</span> documentation mentions Next.js',
+        timestamp: "2023-04-04T23:40:39Z",
+      },
+    ],
+  },
+};
